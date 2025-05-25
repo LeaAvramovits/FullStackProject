ecc3be550aa69b08dc7fb91b606c6bd60a9602b1
LeaAvramovits <etty54942@gmail.com>
2025-05-25 14:27:05 +0300

    add new file for trying merge

---
+++ backend/BL/API/newFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.API
+{
+    internal class newFile
+    {
+        public newFile()
+        {
+            // This class is intentionally left empty.
+            // It serves as a placeholder for future code or documentation.
+        }
+    }
+}
